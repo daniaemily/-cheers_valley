a670868816c49fd81e9f3364f15ceea168d11831
Dania Maciel <daniaemily@gmail.com>
2018-07-23 12:20:04 -0400

    wineries are geocoded

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ ruby '2.4.4'
 
 gem 'bootsnap', require: false
 gem 'devise'
+gem "geocoder"
 gem 'jbuilder', '~> 2.0'
 gem 'pg', '~> 0.21'
 gem 'puma'
--- app/models/winery.rb
+++ app/models/winery.rb
@@ -1,3 +1,5 @@
 class Winery < ApplicationRecord
   # validates :name, presence :true
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 end
