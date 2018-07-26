5011407a5997afcd08e759758c7fc60ae4021a32
Dania Maciel <daniaemily@gmail.com>
2018-07-26 11:34:29 -0400

    search functionality, fixed map markers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'wineries#index'
 
-  resources :wineries
+  resources :wineries do
+    collection do
+      get :autocomplete
+    end
+  end
   #only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
